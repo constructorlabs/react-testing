1abc7773d421034cc50085e7250601365a6a402b
dgrabov <d.grabov@gmail.com>
2018-03-07 13:54:24 +0000

    create mock fetch test

---
+++ tests/DisplayMovie.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import fetchMock from 'fetch-mock';
+import {shallow} from 'enzyme';
+import DisplayMovie from '../src/DisplayMovie';
+
+describe('Counter component', () => {
+  describe('fetch and display', () => {
+    beforeEach(() => {
+      fetchMock.get('*', {
+        Title:'My awesome movie'
+      });
+    });
+
+    it('increments counter', done => {
+      const wrapper = shallow(<DisplayMovie />);
+      const button = wrapper.find('button').simulate('click');
+
+      setTimeout(() => {
+        const update = wrapper.update();
+        console.log(update.html());
+        const text = update.find('h1').text();
+        expect(text).toEqual('My awesome movie');
+        done();
+      }, 0);
+    });
+
+    afterEach(() => {
+      fetchMock.restore();
+    });
+  });
+});
